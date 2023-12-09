1a879850873bb91f83ed4dce22fd23ef1a98e25d
Romello <romello.ten.broeke@hva.nl>
2023-12-09 08:43:37 +0100

    Added continuous rotation to the displayed model.

--- src/components/ModelComponent.tsx
+++ src/components/ModelComponent.tsx
@@ -1,12 +1,16 @@
-import React  from "react";
+import React, {useRef, useState} from "react";
 import { useGLTF } from "@react-three/drei";
 import { Vector3, Euler } from "three";
+import {useFrame} from "@react-three/fiber";
+import * as THREE from "three";
+import {is} from "@react-spring/shared";
+
 
 interface ModelProps {
     path: string;
-    position?: Vector3;
-    rotation?: Euler;
-    scale?: Vector3;
+    position: Vector3;
+    rotation: Euler;
+    scale: Vector3;
     isMovable: boolean
 }
 
@@ -14,6 +18,7 @@ const defaultPosition: Vector3 = new Vector3(0, 0, 0);
 const defaultRotation: Euler = new Euler(0, 0, 0);
 const defaultScale: Vector3 = new Vector3(1, 1, 1);
 const defaultMovable: boolean = false;
+const defaultRotationSpeed = 0.005;
 
 function ModelComponent({
                    path,
@@ -24,14 +29,19 @@ function ModelComponent({
                }: ModelProps) {
     const { scene } = useGLTF(path);
 
-    return (
-            <primitive
-                object={scene}
-                position={position}
-                rotation={rotation}
-                scale={scale}
+    useFrame(() => {
+        if (isMovable) {
+            scene.rotation.y += defaultRotationSpeed
+        }
+    })
 
-            />
+
+    return (
+
+            <primitive object={scene} position={position}
+                       rotation={rotation}
+                       scale={scale}/>
+
     );
 }
 
